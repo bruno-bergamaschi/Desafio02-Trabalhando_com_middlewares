35590e72d17cf071c69528026184b24fc4c7a5b3
Bruno <brunobergamaschi@live.com>
2021-11-23 13:50:11 -0300

    Desafio concluído

---
+++ Readme.md
@@ -0,0 +1,23 @@
+Testes
+
+[v] Should be able to find user by username in header and pass it to request.user
+
+[v] Should not be able to find a non existing user by username in header
+
+[v] Should be able to let user create a new todo when is in free plan and have less than ten todos
+
+[v] Should not be able to let user create a new todo when is not Pro and already have ten todos
+
+[v] Should be able to let user create infinite new todos when is in Pro plan
+
+[v] Should be able to put user and todo in request when both exits
+
+[v] Should not be able to put user and todo in request when user does not exists
+
+[v] Should not be able to put user and todo in request when todo id is not uuid
+
+[v] Should not be able to put user and todo in request when todo does not exists
+
+[v] Should be able to find user by id route param and pass it to request.user
+
+[v] Should not be able to pass user to request.user when it does not exists
--- src/index.js
+++ src/index.js
@@ -9,122 +9,182 @@ app.use(cors());
 
 const users = [];
 
+// Esse middleware é responsável por receber o username do usuário pelo header e validar se existe ou não um usuário com o username passado. Caso exista, o usuário deve ser repassado para o request e a função next deve ser chamada.
+
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+	const { username } = request.headers;
+
+	const user = users.find((user) => user.username === username);
+
+	if (!user) return response.status(404).json({ error: 'User not found' });
+
+	request.user = user;
+
+	return next();
 }
+
+// Esse middleware deve receber o usuário já dentro do request e chamar a função next apenas se esse usuário ainda estiver no plano grátis e ainda não possuir 10 todos cadastrados ou se ele já estiver com o plano Pro ativado.
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+	const { user } = request;
+
+	if (user.pro === false && user.todos.length === 10) {
+		return response.status(403).json({ error: 'limit of entries exceeded' });
+	}
+
+	return next();
 }
+
+// Esse middleware deve receber o **username** de dentro do header e o **id** de um *todo* de dentro de `request.params`. Você deve validar o usuário, validar que o `id` seja um uuid e também validar que esse `id` pertence a um *todo* do usuário informado.
+
+// Com todas as validações passando, o *todo* encontrado deve ser passado para o `request` assim como o usuário encontrado também e a função next deve ser chamada.
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+	const { username } = request.headers;
+	const { id } = request.params;
+	let todo;
+
+	const user = users.find((userIndex) => userIndex.username === username);
+	const validateID = validate(id);
+
+	if (user === undefined) {
+		return response.status(404).json({ error: 'User not found' });
+	} else {
+		todo = user.todos.find((todoIndex) => todoIndex.id === id);
+	}
+
+	if (!validateID) {
+		return response.status(400).json({ error: 'ID invalid' });
+	}
+
+	if (todo === undefined) {
+		return response.status(404).json({ error: 'Todo not found' });
+	}
+
+	if (user != undefined && todo != undefined) {
+		request.user = user;
+		request.todo = todo;
+
+		return next();
+	}
 }
 
+// Esse middleware possui um funcionamento semelhante ao middleware checksExistsUserAccount mas a busca pelo usuário deve ser feita através do id de um usuário passado por parâmetro na rota. Caso o usuário tenha sido encontrado, o mesmo deve ser repassado para dentro do request.user e a função next deve ser chamada.
+
 function findUserById(request, response, next) {
-  // Complete aqui
+	const { id } = request.params;
+
+	const user = users.find((user) => user.id === id);
+
+	if (!user) {
+		return response.status(404).json({ error: 'User not found' });
+	}
+
+	request.user = user;
+
+	return next();
 }
 
 app.post('/users', (request, response) => {
-  const { name, username } = request.body;
+	const { name, username } = request.body;
 
-  const usernameAlreadyExists = users.some((user) => user.username === username);
+	const usernameAlreadyExists = users.some((user) => user.username === username);
 
-  if (usernameAlreadyExists) {
-    return response.status(400).json({ error: 'Username already exists' });
-  }
+	if (usernameAlreadyExists) {
+		return response.status(400).json({ error: 'Username already exists' });
+	}
 
-  const user = {
-    id: uuidv4(),
-    name,
-    username,
-    pro: false,
-    todos: []
-  };
+	const user = {
+		id: uuidv4(),
+		name,
+		username,
+		pro: false,
+		todos: []
+	};
 
-  users.push(user);
+	users.push(user);
 
-  return response.status(201).json(user);
+	return response.status(201).json(user);
 });
 
 app.get('/users/:id', findUserById, (request, response) => {
-  const { user } = request;
+	const { user } = request;
 
-  return response.json(user);
+	return response.json(user);
 });
 
 app.patch('/users/:id/pro', findUserById, (request, response) => {
-  const { user } = request;
+	const { user } = request;
 
-  if (user.pro) {
-    return response.status(400).json({ error: 'Pro plan is already activated.' });
-  }
+	if (user.pro) {
+		return response.status(400).json({ error: 'Pro plan is already activated.' });
+	}
 
-  user.pro = true;
+	user.pro = true;
 
-  return response.json(user);
+	return response.json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  const { user } = request;
+	const { user } = request;
 
-  return response.json(user.todos);
+	return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, checksCreateTodosUserAvailability, (request, response) => {
-  const { title, deadline } = request.body;
-  const { user } = request;
+	const { title, deadline } = request.body;
+	const { user } = request;
 
-  const newTodo = {
-    id: uuidv4(),
-    title,
-    deadline: new Date(deadline),
-    done: false,
-    created_at: new Date()
-  };
+	const newTodo = {
+		id: uuidv4(),
+		title,
+		deadline: new Date(deadline),
+		done: false,
+		created_at: new Date()
+	};
 
-  user.todos.push(newTodo);
+	user.todos.push(newTodo);
 
-  return response.status(201).json(newTodo);
+	return response.status(201).json(newTodo);
 });
 
 app.put('/todos/:id', checksTodoExists, (request, response) => {
-  const { title, deadline } = request.body;
-  const { todo } = request;
+	const { title, deadline } = request.body;
+	const { todo } = request;
 
-  todo.title = title;
-  todo.deadline = new Date(deadline);
+	todo.title = title;
+	todo.deadline = new Date(deadline);
 
-  return response.json(todo);
+	return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksTodoExists, (request, response) => {
-  const { todo } = request;
+	const { todo } = request;
 
-  todo.done = true;
+	todo.done = true;
 
-  return response.json(todo);
+	return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, checksTodoExists, (request, response) => {
-  const { user, todo } = request;
+	const { user, todo } = request;
 
-  const todoIndex = user.todos.indexOf(todo);
+	const todoIndex = user.todos.indexOf(todo);
 
-  if (todoIndex === -1) {
-    return response.status(404).json({ error: 'Todo not found' });
-  }
+	if (todoIndex === -1) {
+		return response.status(404).json({ error: 'Todo not found' });
+	}
 
-  user.todos.splice(todoIndex, 1);
+	user.todos.splice(todoIndex, 1);
 
-  return response.status(204).send();
+	return response.status(204).send();
 });
 
 module.exports = {
-  app,
-  users,
-  checksExistsUserAccount,
-  checksCreateTodosUserAvailability,
-  checksTodoExists,
-  findUserById
+	app,
+	users,
+	checksExistsUserAccount,
+	checksCreateTodosUserAvailability,
+	checksTodoExists,
+	findUserById
 };
